87e59da24a77a870b508dc56c7c3e646ca439441
NivyaMV <91795070+NivyaMV@users.noreply.github.com>
2021-10-03 13:15:55 +0530

    Add files via upload

---
+++ AssignmentPositiveNegative.java
@@ -0,0 +1,18 @@
+package week1.day1;
+
+public class AssignmentPositiveNegative {
+
+	public static void main(String[] args) {
+		int numb=0;
+		if(numb==0) {
+			System.out.println("The given number is zero");
+		}
+		else if(numb>0) {
+			System.out.println(numb + " is a Positive Number");
+		}
+		else {
+			System.out.println(numb + " is a Negative Number");
+		}
+	}
+
+}
