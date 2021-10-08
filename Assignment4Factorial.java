cf515bece0385bb708670da4b9f8807a9b22e96e
NivyaMV <91795070+NivyaMV@users.noreply.github.com>
2021-10-08 16:57:04 +0530

    Add files via upload

---
+++ Assignment4Factorial.java
@@ -0,0 +1,24 @@
+package week1.day1;
+public class Assignment4Factorial 
+{
+	public static void main(String[] args) 
+	{
+
+		int x=6;		
+		int fact=1;
+		for(int i=1;i<=x;i++) 
+		{
+			fact=i*fact;
+		}
+		System.out.println("The factorial is " +fact );	
+	}
+}
+
+
+
+
+
+
+
+
+
